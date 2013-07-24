7572b5f87d8d391948ebf6623f4032b22f327798
Laine <laine.herron@10gen.com>
2013-07-24 10:28:24 -0700

    fixed some formatting.  now lines <= 80 characters

--- python_testing.py
+++ python_testing.py
@@ -38,22 +38,26 @@ def takes(*args_types, **kwargs_types):
             if not len(args) == len(args_types):
                 error = True
                 errormsg += ('"takes()" expected function "'+f.__name__
-                             +'" to receive '+str(len(args_types))
-                             +' arguments, but '+str(len(args))+' were given.\n')
+                            +'" to receive '+str(len(args_types))
+                            +' arguments, but '+str(len(args))+' were given.\n')
             for i in range(min(len(args),len(args_types))):
                 if not type(args[i]) == args_types[i]:
                     error = True
-                    errormsg += ('"takes()" expected parameter #'+str(i+1)+' of function "'
-                                 +f.__name__+'" to be type '+str(args_types[i])
-                                 +' but it received  type '+str(type(args[i]))
-                                 +' with value: '+str(args[i])+'\n')
+                    errormsg += ('"takes()" expected parameter #'+str(i+1)
+                                +' of function "'+f.__name__+'" to be type '
+                                +str(args_types[i])+' but it received  type '
+                                +str(type(args[i]))+' with value: '
+                                +str(args[i])+'\n')
             for argname in kwargs_types:
                 if argname in kwargs:
                     if not type(kwargs[argname]) == kwargs_types[argname]:
                         error = True
-                        errormsg += ('takes() expected kwarg "'+str(argname)+'" of "'
-                        +f.__name__+'" to have type '+str(kwargs_types[argname])+' but received type '
-                        +str(type(kwargs[argname]))+' with value: '+str(kwargs[argname]))
+                        errormsg += ('takes() expected kwarg "'+str(argname)
+                                    +'" of "'+f.__name__+'" to have type '
+                                    +str(kwargs_types[argname])
+                                    +' but received type '
+                                    +str(type(kwargs[argname]))+' with value: '
+                                    +str(kwargs[argname]))
             if error:
                 raise Exception(errormsg)
             return f(*args, **kwargs)
@@ -62,11 +66,12 @@ def takes(*args_types, **kwargs_types):
 
 
 # acts like print, but also prints code context it was called from
-# usage: dprint(x+2) --> <'dprint: file laine.py, function f, line 0:\n 'x+2' --> 5>
+# usage: dprint(x+2): <'dprint: file file.py, function f, line 0:\n 'x+2' --> 5>
 def dprint(var):
     stack = traceback.extract_stack()
     filename, lineno, function_name, code = stack[-2]
-    print('dprint: file "'+filename+'", function "'+function_name+'", line #'+str(lineno)+':')
+    print('dprint: file "'+filename+'", function "'+function_name
+            +'", line #'+str(lineno)+':')
     l_index = code.index('dprint(')+7
     r_index = code.rindex(')')
     print('\t"'+code[l_index : r_index]+'" --> '+str(var))
